46fcb8277a0c9dbb9a6ce49d08c8a67c6269139f
yongxiangng <yongxiangng@gmail.com>
2021-05-14 12:44:12 +0800

    Change secret key and hide secret key

--- website_project/urls.py
+++ website_project/urls.py
@@ -1,4 +1,4 @@
-"""helloworld_project URL Configuration
+"""website_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
